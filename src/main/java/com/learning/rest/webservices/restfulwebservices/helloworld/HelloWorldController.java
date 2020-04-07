99b3f9ab54f6bfee050aeef9e52df6149b320848
ashish <ashish@ashishs-mbp>
2020-04-07 00:07:59 -0500

    Changes for i18n by removing @RequestHeader annotation

--- src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/learning/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -34,5 +35,10 @@ public class HelloWorldController {
 	public String helloworldinternationalized(
 			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
 		return messageSource.getMessage("good.morning.message", null, locale);
+	}
+
+	@GetMapping(path = "/hello-world-internationalized-remove-header")
+	public String helloworldinternationalizedremoveheader() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
